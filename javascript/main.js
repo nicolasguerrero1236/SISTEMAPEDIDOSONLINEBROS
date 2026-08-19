5f3dded8c4b63e6c06fe49196ce45e642487e439
Nico Guerrero PC Dani <nicolasguerrero1236@gmail.com>
2026-08-19 20:11:05 -0300

    precio delivery

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     WHATSAPP_NUMBER: '543518697090',
     NEGOCIO: 'Bros Burger y Lomos',
-    COSTO_DELIVERY: 1000
+    COSTO_DELIVERY: 2000
 };
 
 const STORAGE_CLIENTE_KEY = 'datos_cliente';
